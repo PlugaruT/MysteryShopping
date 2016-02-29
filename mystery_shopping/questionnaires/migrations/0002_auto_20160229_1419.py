90dfbb57178d9e68be61bf4425b2d38ee8e6441f
iulian <iulian.gulea@gmail.com>
2016-02-29 16:21:10 +0200

    Added migration.

---
+++ mystery_shopping/questionnaires/migrations/0002_auto_20160229_1419.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.contrib.postgres.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('questionnaires', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='questionnairequestion',
+            name='answer_choices',
+            field=django.contrib.postgres.fields.ArrayField(blank=True, null=True, base_field=models.IntegerField(), size=None),
+        ),
+    ]
