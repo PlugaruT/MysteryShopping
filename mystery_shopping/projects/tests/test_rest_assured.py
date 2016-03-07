e368283eb31667f9a227a1de5a51115504daca2f
Mihai Coslet <mcosletjr@gmail.com>
2016-03-07 23:39:54 +0200

    Add test questionnaire if it gets a score of 100.

--- mystery_shopping/projects/tests/test_rest_assured.py
+++ mystery_shopping/projects/tests/test_rest_assured.py
@@ -1,4 +1,5 @@
 import json
+from decimal import Decimal
 from datetime import datetime
 
 from rest_assured.testcases import CreateAPITestCaseMixin
@@ -6,7 +7,10 @@ from rest_assured.testcases import ReadWriteRESTAPITestCaseMixin
 from rest_assured.testcases import BaseRESTAPITestCase
 from factory.fuzzy import FuzzyDateTime
 
-from mystery_shopping.questionnaires.serializers import QuestionnaireTemplate
+from mystery_shopping.questionnaires.models import Questionnaire
+
+from mystery_shopping.questionnaires.serializers import QuestionnaireTemplateSerializer
+from mystery_shopping.projects.serializers import EvaluationSerializer
 from mystery_shopping.projects.project_statuses import ProjectStatus
 
 from mystery_shopping.factories.questionnaires import QuestionnaireTemplateFactory
@@ -43,7 +47,38 @@ class EvaluationAPITestCase(CreateAPITestCaseMixin, BaseRESTAPITestCase):
         kwargs['format'] = 'json'
         super(EvaluationAPITestCase, self).test_create(data, **kwargs)
 
-    def _test_questionnaire_score_100(self):
-        questionnaire_json = json.load(open("mystery_shopping/questionnaires/tests/QuestionnaireTemplates.json"))
+    def test_questionnaire_score_100(self):
+        template_questionnaire_json_data = json.load(open("mystery_shopping/questionnaires/tests/QuestionnaireTemplates.json"))
+        template_questionnaire_json = template_questionnaire_json_data[2]
         tenant = TenantFactory()
-        questionnaire_json['tenant'] = tenant.id
+        template_questionnaire_json['tenant'] = tenant.id
+        template_questionnaire_ser = QuestionnaireTemplateSerializer(data=template_questionnaire_json)
+        template_questionnaire_ser.is_valid(raise_exception=True)
+        template_questionnaire_ser.save()
+
+        evaluation_data = {
+            "evaluation_type": "visit",
+            "is_draft": False,
+            "suggested_start_date": datetime(2008, 1, 1),
+            "suggested_end_date": datetime(2016, 1, 1),
+            "status": ProjectStatus.PLANNED,
+            "time_accomplished": None,
+            "project": self.object.project.id,
+            "shopper": self.object.shopper.id,
+            "questionnaire_script": self.object.questionnaire_script.id,
+            "questionnaire_template": template_questionnaire_ser.instance.id,
+            "entity": self.object.entity.id,
+            "evaluation_assessment_level": None
+        }
+        evaluation_ser = EvaluationSerializer(data=evaluation_data)
+        evaluation_ser.is_valid(raise_exception=True)
+        evaluation_ser.save()
+        questionnaire = Questionnaire.objects.get(pk=evaluation_ser.data['questionnaire']['id'])
+        for question in questionnaire.questions.all():
+            for question_choice in question.question_choices.all():
+                if question_choice.text in {'Da', 'A', 'Adevar', 'Yes'}:
+                    question.answer_choices = [question_choice.id]
+                    question.save()
+
+        questionnaire.calculate_score()
+        self.assertEqual(questionnaire.score, Decimal(100))
