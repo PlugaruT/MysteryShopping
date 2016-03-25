bb83ba041704dfd35b40de77f18dd27d9ed7c12a
iulian <iulian.gulea@gmail.com>
2016-03-25 14:52:27 +0200

    Added merged migration.

---
+++ mystery_shopping/questionnaires/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('questionnaires', '0007_questionnairetemplatequestion_show_comment'),
+        ('questionnaires', '0007_auto_20160324_1346'),
+    ]
+
+    operations = [
+    ]
