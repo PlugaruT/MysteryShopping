183efacbb3e0d3a025d52f0dc9527a6308cccc46
Mihai Coslet <mcosletjr@gmail.com>
2016-05-10 17:12:13 +0300

    Cover with tests sort_indicator_question_marks and add_question_per_coded_cause methods.

--- mystery_shopping/nps/tests/test_algorithms.py
+++ mystery_shopping/nps/tests/test_algorithms.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from decimal import Decimal
 from json import load
+from random import randint
 from unittest.mock import MagicMock
 
 from django.test import TestCase
@@ -8,9 +9,12 @@ from django.test import TestCase
 from ..algorithms import calculate_indicator_score
 from ..algorithms import create_details_skeleton
 from ..algorithms import get_indicator_scores
+from ..algorithms import sort_indicator_question_marks
+from ..algorithms import add_question_per_coded_cause
 
 from mystery_shopping.questionnaires.serializers import QuestionnaireTemplateSerializer
 from mystery_shopping.questionnaires.constants import IndicatorQuestionType
+from mystery_shopping.questionnaires.constants import QuestionType
 from mystery_shopping.factories.tenants import TenantFactory
 
 
@@ -127,3 +131,86 @@ class AlgorithmsTestCase(TestCase):
                 self.assertIn(medium_key, test_indicator_skeleton[big_key])
                 for small_key in indicator_skeleton[big_key][medium_key]:
                     self.assertIn(small_key, test_indicator_skeleton[big_key][medium_key])
+
+    def test_sort_indicator_question_marks_with_answer_choices(self):
+        indicator_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+        mark = Decimal(10.00)
+        answer_choice = 123
+
+        indicator_question = MagicMock()
+        indicator_question.type = IndicatorQuestionType.INDICATOR_QUESTION
+        indicator_question.score = mark
+
+        question = MagicMock()
+        question.type = QuestionType.SINGLE_CHOICE
+        question.answer = 'Romanian'
+        question.question_body = 'Language'
+        question.answer_choices = answer_choice
+
+        sort_indicator_question_marks(indicator_dict, indicator_question, question)
+
+        self.assertEqual(indicator_dict[question.question_body][question.answer]['marks'][0], mark)
+
+    def test_sort_indicator_question_marks_with_no_answer_choices(self):
+        indicator_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+        mark = Decimal(10.00)
+        answer_choice = []
+
+        indicator_question = MagicMock()
+        indicator_question.type = IndicatorQuestionType.INDICATOR_QUESTION
+        indicator_question.score = mark
+
+        question = MagicMock()
+        question.type = QuestionType.SINGLE_CHOICE
+        question.answer = 'Romanian'
+        question.question_body = 'Language'
+        question.answer_choices = answer_choice
+
+        sort_indicator_question_marks(indicator_dict, indicator_question, question)
+
+        self.assertEqual(indicator_dict[question.question_body]['other']['marks'][0], mark)
+
+    def test_sort_indicator_question_marks_with_with_indicator_question(self):
+        indicator_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+        mark = Decimal(10.00)
+        answer_choice = []
+
+        indicator_question = MagicMock()
+        indicator_question.type = IndicatorQuestionType.INDICATOR_QUESTION
+        indicator_question.score = mark
+
+        question = MagicMock()
+        question.type = IndicatorQuestionType.INDICATOR_QUESTION
+        question.answer = 'lalala'
+        question.question_body = 'NPS'
+        question.answer_choices = answer_choice
+
+        sort_indicator_question_marks(indicator_dict, indicator_question, question)
+
+        self.assertFalse(indicator_dict[question.question_body]['other']['marks'])
+
+    def test_add_question_per_coded_cause_with_cc(self):
+        coded_causes_dict = defaultdict(list)
+
+        cc_id = randint(1, 100)
+        coded_cause = MagicMock()
+        coded_cause.id = cc_id
+
+        indicator_question_id = randint(1, 100)
+        indicator_question = MagicMock()
+        indicator_question.id = indicator_question_id
+        indicator_question.coded_causes.first.return_value = coded_cause
+
+        add_question_per_coded_cause(indicator_question, coded_causes_dict)
+        self.assertEqual(coded_causes_dict[coded_cause.id][0], indicator_question_id)
+
+    def test_add_question_per_coded_cause_without_cc(self):
+        coded_causes_dict = defaultdict(list)
+
+        indicator_question_id = randint(1, 100)
+        indicator_question = MagicMock()
+        indicator_question.id = indicator_question_id
+        indicator_question.coded_causes.first.return_value = None
+
+        add_question_per_coded_cause(indicator_question, coded_causes_dict)
+        self.assertEqual(coded_causes_dict['unsorted'][0], indicator_question_id)
