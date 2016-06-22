44329b4900cde2434f80ea38c53ca1770a9bd905
Mihai Coslet <mcosletjr@gmail.com>
2016-06-22 15:56:27 +0300

    Add 'published' field to dashboard tempaltes.

--- mystery_shopping/dashboard/models.py
+++ mystery_shopping/dashboard/models.py
@@ -17,6 +17,7 @@ class DashboardTemplate(models.Model):
     # Attributes
     title = models.CharField(max_length=120)
     widgets = models.TextField()
+    published = models.BooleanField(default=False)
 
     def __str__(self):
         return 'Dashboard "{}" for {}'.format(self.title, self.tenant.name)
