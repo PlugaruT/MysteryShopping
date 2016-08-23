94341385088f07295220e710c62a2d913257cb97
Mihai Coslet <mcosletjr@gmail.com>
2016-08-23 19:46:12 +0300

    Remove some more stuff.

--- mystery_shopping/mystery_shopping_utils/management/commands/createadmin.py
+++ mystery_shopping/mystery_shopping_utils/management/commands/createadmin.py
@@ -6,5 +6,5 @@ from mystery_shopping.users.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(email="iulian.gulea@sparklabs.md").exists():
-            User.objects.create_superuser('iulian.gulea', "iulian.gulea@sparklabs.md", "gulea")
+        if not User.objects.filter(email="mystery.user@sparklabs.md").exists():
+            User.objects.create_superuser('mystery.user', "mystery.user@sparklabs.md", "mystery")
