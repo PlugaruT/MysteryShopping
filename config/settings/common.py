6181fc3aa406c2f485fb92cb3811345b31e62fa1
Mark Graciov <mark.graciov@gmail.com>
2016-12-14 22:53:58 +0200

    enable basic auth only for local config

--- config/settings/common.py
+++ config/settings/common.py
@@ -256,16 +256,12 @@ CORS_ALLOW_CREDENTIALS = True
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    )
-#     'DEFAULT_PARSER_CLASSES': (
-#         'rest_framework.parsers.JSONParser',
-#     )
+    ]
 }
 
 
