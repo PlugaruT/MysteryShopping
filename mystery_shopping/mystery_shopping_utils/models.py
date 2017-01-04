be2b76e27870571355d94d56291e1bbbc0402b00
Mihai Coslet <mcosletjr@gmail.com>
2017-01-04 10:00:16 +0200

    Add mixin and filter for tenant.

--- mystery_shopping/mystery_shopping_utils/models.py
+++ mystery_shopping/mystery_shopping_utils/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from rest_framework.filters import BaseFilterBackend
+
+from mystery_shopping.tenants.models import Tenant
+
+
+class HasTenantMixin(models.Model):
+    """
+    Generic abstract class model that has a reference to a `tenant`
+    and some helpful methods on `tenant` handling.
+    """
+    tenant = models.ForeignKey(Tenant)
+
+    class Meta:
+        abstract = True
+
+
+class TenantFilter(BaseFilterBackend):
+    """
+    Generic Filter to use on all views whose models have a tenant
+     reference.
+    """
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(tenant=request.user.tenant)
