37696635c5838e4337811ff2678360d0097d0f96
PlugaruT <plugaru.tudor@gmail.com>
2017-04-05 22:19:10 +0300

    add cors origin whitelist

--- config/settings/common.py
+++ config/settings/common.py
@@ -253,7 +253,7 @@ CORS_ORIGIN_WHITELIST = (
     'localhost:3000'
 )
 
-CORS_ORIGIN_REGEX_WHITELIST = ('^(https?://)?(\w+\.)?dapi\.solutions', )
+CORS_ORIGIN_REGEX_WHITELIST = ('^(https?://)?(\w+\.)?dapi\.solutions', '^(https?://)?(\w+\.)?cloudfront\.net')
 
 CORS_ALLOW_CREDENTIALS = True
 
