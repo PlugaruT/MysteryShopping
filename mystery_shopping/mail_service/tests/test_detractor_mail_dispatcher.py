958bf895aebabb012316115e5defcacc1e2d656c
PlugaruT <plugaru.tudor@gmail.com>
2017-10-03 11:44:55 +0300

    add more tests, should be improved after defining templates for email

--- mystery_shopping/mail_service/tests/test_detractor_mail_dispatcher.py
+++ mystery_shopping/mail_service/tests/test_detractor_mail_dispatcher.py
@@ -1,3 +1,4 @@
+from django.core import mail
 from django.test import TestCase
 from mystery_shopping.mail_service.mail import DetractorEmailDispatcher
 
@@ -6,14 +7,21 @@ class TestDetractorEmailDispatcher(TestCase):
     def setUp(self):
         self.DEFAULT_FROM_EMAIL = 'test@sparklabs.md'
 
-    def test_init_with_one_recipient(self):
+    def test_init_with_one_recipient_as_str(self):
         to_email = 'user@email.com'
 
         service = DetractorEmailDispatcher(to_email)
 
         self.assertListEqual(service.recipients, [to_email])
 
-    def test_init_with_more_recipients(self):
+    def test_init_with_one_recipient_as_list(self):
+        to_email = ['user@email.com']
+
+        service = DetractorEmailDispatcher(to_email)
+
+        self.assertListEqual(service.recipients, to_email)
+
+    def test_init_with_more_recipients_as_list(self):
         to_emails = ['user@email.com', 'user2@email.com']
 
         service = DetractorEmailDispatcher(to_emails)
@@ -21,7 +29,15 @@ class TestDetractorEmailDispatcher(TestCase):
         self.assertListEqual(service.recipients, to_emails)
 
     def test_send_new_detractor_email(self):
-        pass
+        to_email = 'user@email.com'
+
+        with self.settings(DEFAULT_FROM_EMAIL=self.DEFAULT_FROM_EMAIL):
+            service = DetractorEmailDispatcher(to_email)
+            service.send_new_detractor_email()
+
+            self.assertEqual(len(mail.outbox), 1)
+            self.assertEqual(mail.outbox[0].from_email, self.DEFAULT_FROM_EMAIL)
+            self.assertEqual(mail.outbox[0].to, [to_email])
 
     def test_send_new_detractors_case_email(self):
         pass
