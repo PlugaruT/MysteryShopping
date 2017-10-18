bc7cd1593623c719dc729ffe8ae7a076848d473d
PlugaruT <plugaru.tudor@gmail.com>
2017-10-18 16:23:40 +0300

    improve admin views for models

--- mystery_shopping/respondents/admin.py
+++ mystery_shopping/respondents/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 
-from mystery_shopping.respondents.models import Respondent
+from mystery_shopping.respondents.models import Respondent, RespondentCase
 
 
 @admin.register(Respondent)
 class RespondentAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('number_of_questions',)
+    list_display = ('view_place', 'number_of_questions')
+
+    def view_place(self, obj):
+        return obj.get_visited_place().element_name
+
+
+@admin.register(RespondentCase)
+class RespondentCaseAdmin(admin.ModelAdmin):
+    list_filter = ('state', 'follow_up_date')
+    list_display = ('view_place', 'state', 'created')
+
+    def view_place(self, obj):
+        return obj.respondent.get_visited_place().element_name
