d422ce490dccb9a4b2b846859bca7251b36ec82f
PlugaruT <plugaru.tudor@gmail.com>
2017-10-25 14:13:57 +0300

    refactor a little bit the email stuff, isolate stuff related to detractor in a new module

--- mystery_shopping/mail_service/mail.py
+++ mystery_shopping/mail_service/mail.py
@@ -1,47 +1,24 @@
 import logging
 from smtplib import SMTPRecipientsRefused
-
+from django.template.loader import get_template
 from django.core.mail import get_connection
 from django.core.mail.message import EmailMultiAlternatives
-from django.template.loader import get_template
 
 log = logging.getLogger(__name__)
 
 
-class DetractorEmailDispatcher:
-    def __init__(self, recipients):
+class EmailDispatcher:
+    def __init__(self, recipients, text_content, html_content, subject):
         if isinstance(recipients, str):
             recipients = [recipients]
         self.recipients = recipients
+        self.text_content = text_content
+        self.html_content = html_content
+        self.subject = subject
 
-    def send_new_detractor_email(self):
-        subject_line = 'New detractor in the --place name--'
-
-        context = {
-            'random': 'random'
-        }
-
-        self._build_and_send_email('new_detractor', context, subject_line)
-
-        return True
-
-    def send_new_detractor_case_email(self):
-        subject_line = 'asdasd'
-        context = {
-            "random": "random"
-        }
-
-        self._build_and_send_email('new_detractor_case', context, subject_line)
-
-        return True
-
-    def _build_and_send_email(self, template_name, context, subject_line):
-        template_name = 'detractors/{}'.format(template_name)
-        text_content = self.get_content_for_template(template_name + '.txt', context)
-        html_content = self.get_content_for_template(template_name + '.html', context)
-
-        email = EmailMultiAlternatives(subject=subject_line, body=text_content, to=self.recipients)
-        email.attach_alternative(html_content, 'text/html')
+    def build_and_send(self):
+        email = EmailMultiAlternatives(subject=self.subject, body=self.text_content, to=self.recipients)
+        email.attach_alternative(self.html_content, 'text/html')
 
         with get_connection() as connection:
             if not self._send_email(connection, email):
@@ -54,7 +31,26 @@ class DetractorEmailDispatcher:
         except SMTPRecipientsRefused:
             log.error('Could not send email to {}'.format(email.to))
 
-    @staticmethod
-    def get_content_for_template(template_path, context):
-        text_template = get_template(template_path)
-        return text_template.render(context)
+
+def get_text_and_html_content(template_path, context):
+    """
+    Method that returns the contents of a template in plain text and html
+    :param template_path: string of the form 'folder_name/template_name', without file extension
+    :param context: dict that contains values that should be passed to the template
+    :return: tuple with text content and html content of the template
+    """
+    text_content = get_content_for_template('{}.txt'.format(template_path), context)
+    html_content = get_content_for_template('{}.html'.format(template_path), context)
+    return text_content, html_content
+
+
+def get_content_for_template(template_path, context):
+    """
+    Method that returns the template with the values from context
+    :param template_path: string of the form 'folder_name/template_name.html', should contain the file extenstion
+    :param context: dict that contains values that should be passed to the template
+    :return: rendered template with passed context
+    """
+
+    text_template = get_template(template_path)
+    return text_template.render(context)
